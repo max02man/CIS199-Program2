d31c3e1808cf695b67d64ad45368c4d6bb5f3d09
max02man <max02man@gmail.com>
2016-11-04 01:50:26 -0400

    Program2
    Priority Early registration for UofL

---
+++ Program2/Program2/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Program2
+{
+    partial class program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result_label = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FRESHMAN_radioButton = new System.Windows.Forms.RadioButton();
+            this.SOPHOMORE_radioButton = new System.Windows.Forms.RadioButton();
+            this.JUNIOR_radioButton = new System.Windows.Forms.RadioButton();
+            this.SENIOR_radioButton = new System.Windows.Forms.RadioButton();
+            this.year_groupBox = new System.Windows.Forms.GroupBox();
+            this.firstletter_textBox = new System.Windows.Forms.TextBox();
+            this.Result_button = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.year_groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "University of Louisville";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(27, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "What year are you:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(174, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "What\'s the first letter of your last name:";
+            // 
+            // result_label
+            // 
+            this.result_label.BackColor = System.Drawing.Color.Red;
+            this.result_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.result_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_label.Location = new System.Drawing.Point(176, 106);
+            this.result_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.result_label.Name = "result_label";
+            this.result_label.Size = new System.Drawing.Size(279, 54);
+            this.result_label.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(2, 39);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(485, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Priority Early Registration Schedule for Spring 2015";
+            // 
+            // FRESHMAN_radioButton
+            // 
+            this.FRESHMAN_radioButton.AutoSize = true;
+            this.FRESHMAN_radioButton.Location = new System.Drawing.Point(13, 22);
+            this.FRESHMAN_radioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.FRESHMAN_radioButton.Name = "FRESHMAN_radioButton";
+            this.FRESHMAN_radioButton.Size = new System.Drawing.Size(85, 17);
+            this.FRESHMAN_radioButton.TabIndex = 10;
+            this.FRESHMAN_radioButton.TabStop = true;
+            this.FRESHMAN_radioButton.Text = "FRESHMAN";
+            this.FRESHMAN_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // SOPHOMORE_radioButton
+            // 
+            this.SOPHOMORE_radioButton.AutoSize = true;
+            this.SOPHOMORE_radioButton.Location = new System.Drawing.Point(13, 44);
+            this.SOPHOMORE_radioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SOPHOMORE_radioButton.Name = "SOPHOMORE_radioButton";
+            this.SOPHOMORE_radioButton.Size = new System.Drawing.Size(95, 17);
+            this.SOPHOMORE_radioButton.TabIndex = 11;
+            this.SOPHOMORE_radioButton.TabStop = true;
+            this.SOPHOMORE_radioButton.Text = "SOPHOMORE";
+            this.SOPHOMORE_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // JUNIOR_radioButton
+            // 
+            this.JUNIOR_radioButton.AutoSize = true;
+            this.JUNIOR_radioButton.Location = new System.Drawing.Point(14, 66);
+            this.JUNIOR_radioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.JUNIOR_radioButton.Name = "JUNIOR_radioButton";
+            this.JUNIOR_radioButton.Size = new System.Drawing.Size(65, 17);
+            this.JUNIOR_radioButton.TabIndex = 12;
+            this.JUNIOR_radioButton.TabStop = true;
+            this.JUNIOR_radioButton.Text = "JUNIOR";
+            this.JUNIOR_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // SENIOR_radioButton
+            // 
+            this.SENIOR_radioButton.AutoSize = true;
+            this.SENIOR_radioButton.Location = new System.Drawing.Point(14, 88);
+            this.SENIOR_radioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SENIOR_radioButton.Name = "SENIOR_radioButton";
+            this.SENIOR_radioButton.Size = new System.Drawing.Size(66, 17);
+            this.SENIOR_radioButton.TabIndex = 13;
+            this.SENIOR_radioButton.TabStop = true;
+            this.SENIOR_radioButton.Text = "SENIOR";
+            this.SENIOR_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // year_groupBox
+            // 
+            this.year_groupBox.BackColor = System.Drawing.Color.Red;
+            this.year_groupBox.Controls.Add(this.SENIOR_radioButton);
+            this.year_groupBox.Controls.Add(this.FRESHMAN_radioButton);
+            this.year_groupBox.Controls.Add(this.JUNIOR_radioButton);
+            this.year_groupBox.Controls.Add(this.SOPHOMORE_radioButton);
+            this.year_groupBox.Location = new System.Drawing.Point(26, 97);
+            this.year_groupBox.Margin = new System.Windows.Forms.Padding(2);
+            this.year_groupBox.Name = "year_groupBox";
+            this.year_groupBox.Padding = new System.Windows.Forms.Padding(2);
+            this.year_groupBox.Size = new System.Drawing.Size(123, 115);
+            this.year_groupBox.TabIndex = 1;
+            this.year_groupBox.TabStop = false;
+            this.year_groupBox.Text = "What year are you:";
+            // 
+            // firstletter_textBox
+            // 
+            this.firstletter_textBox.BackColor = System.Drawing.Color.Red;
+            this.firstletter_textBox.Location = new System.Drawing.Point(386, 81);
+            this.firstletter_textBox.Margin = new System.Windows.Forms.Padding(2);
+            this.firstletter_textBox.Name = "firstletter_textBox";
+            this.firstletter_textBox.Size = new System.Drawing.Size(76, 20);
+            this.firstletter_textBox.TabIndex = 0;
+            // 
+            // Result_button
+            // 
+            this.Result_button.BackColor = System.Drawing.Color.Red;
+            this.Result_button.Location = new System.Drawing.Point(176, 162);
+            this.Result_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Result_button.Name = "Result_button";
+            this.Result_button.Size = new System.Drawing.Size(90, 50);
+            this.Result_button.TabIndex = 2;
+            this.Result_button.Text = "RESULT";
+            this.Result_button.UseVisualStyleBackColor = false;
+            this.Result_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.BackColor = System.Drawing.Color.Red;
+            this.exit_button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exit_button.Location = new System.Drawing.Point(365, 162);
+            this.exit_button.Margin = new System.Windows.Forms.Padding(2);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(90, 50);
+            this.exit_button.TabIndex = 3;
+            this.exit_button.Text = "EXIT";
+            this.exit_button.UseVisualStyleBackColor = false;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // program2
+            // 
+            this.AcceptButton = this.Result_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CancelButton = this.exit_button;
+            this.ClientSize = new System.Drawing.Size(491, 233);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.Result_button);
+            this.Controls.Add(this.firstletter_textBox);
+            this.Controls.Add(this.year_groupBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.result_label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "program2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Program2";
+            this.year_groupBox.ResumeLayout(false);
+            this.year_groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label result_label;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton FRESHMAN_radioButton;
+        private System.Windows.Forms.RadioButton SOPHOMORE_radioButton;
+        private System.Windows.Forms.RadioButton JUNIOR_radioButton;
+        private System.Windows.Forms.RadioButton SENIOR_radioButton;
+        private System.Windows.Forms.GroupBox year_groupBox;
+        private System.Windows.Forms.TextBox firstletter_textBox;
+        private System.Windows.Forms.Button Result_button;
+        private System.Windows.Forms.Button exit_button;
+    }
+}
+
---
+++ Program2/Program2/Form1.cs
@@ -0,0 +1,184 @@
+﻿//Alaa Hawsawi 
+//CIS199-01  
+//Date:10-27-2014
+//This program will tell you when you register day and time by enter the first letter of your last name and what year they are
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class program2 : Form
+    {
+
+        public program2()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char firstletter;
+            char.TryParse(firstletter_textBox.Text, out firstletter);
+           
+            firstletter=char.ToUpper(firstletter);
+            //time and date for senior
+            if (SENIOR_radioButton.Checked)
+            {
+                result_label.Text = "FRIDAY, NOV 7";
+
+                if (firstletter >= 'A' && firstletter <= 'D')
+                {
+                    result_label.Text += " at 10:00am";
+                }
+                else if (firstletter >= 'E' && firstletter <= 'I')
+                {
+                    result_label.Text += " at 11:30am";
+                }
+                else if (firstletter >= 'J' && firstletter <= 'O')
+                {
+                    result_label.Text += " at 2:00pm";
+                }
+                else if (firstletter >= 'P' && firstletter <= 'R')
+                {
+                    result_label.Text += " at 4:00pm";
+                }
+                else if (firstletter >= 'S' && firstletter <= 'Z')
+                {
+                    result_label.Text += " at 8:30am";
+                }
+            }
+                //time and date for junior
+
+            else if (JUNIOR_radioButton.Checked)
+            {
+                result_label.Text = "MONDAY, NOV 10 ";
+                if (firstletter >= 'A' && firstletter <= 'D')
+                {
+                    result_label.Text += " at 10:00am";
+                }
+                else if (firstletter >= 'E' && firstletter <= 'I')
+                {
+                    result_label.Text += " at 11:30am";
+                }
+                else if (firstletter >= 'J' && firstletter <= 'O')
+                {
+                    result_label.Text += " at 2:00pm";
+                }
+                else if (firstletter >= 'P' && firstletter <= 'R')
+                {
+                    result_label.Text += " at 4:00pm";
+                }
+                else if (firstletter >= 'S' && firstletter <= 'Z')
+                {
+                    result_label.Text += " at 8:30am";
+                }
+            }
+                //date for sophomore
+            else if (SOPHOMORE_radioButton.Checked)
+            {
+                result_label.Text = "TUESDAY, NOV ";
+                if (firstletter >= 'E' && firstletter <= 'R')                
+                 result_label.Text += "12 ";                
+                else 
+                 result_label.Text += "11 ";
+            }
+            //date for freachman
+            else
+            {
+                result_label.Text = "THURSDAY, NOV ";
+              if (firstletter >= 'E' && firstletter <= 'R')                
+                 result_label.Text += "13 ";                
+                else 
+                 result_label.Text += "14 ";            
+            }
+                //time freachman and saphmore
+                if (firstletter >= 'S' && firstletter <= 'T')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 8:30am";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 8:30am";
+                }
+                else if (firstletter >= 'U' && firstletter <= 'V')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 10:00am";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 10:00am";
+                }
+                else if (firstletter >= 'W' && firstletter <= 'Z')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 11:30am";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 11:30am";
+                }
+                else if (firstletter >= 'A' && firstletter <= 'B')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 2:00pm";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 2:00pm";
+                }
+                else if (firstletter >= 'C' && firstletter <= 'D')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 4:00pm";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 4:00pm";
+                }
+                else if (firstletter >= 'E' && firstletter <= 'F')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 8:30am";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 8:30am";
+                }
+                else if (firstletter >= 'G' && firstletter <= 'I')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += " at 10:00am";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 10:00am";
+                }
+                else if (firstletter >= 'J' && firstletter <= 'L')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 11:30am";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 11:30am";
+                }
+                else if (firstletter >= 'M' && firstletter <= 'O')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 2:00pm";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 2:00pm";
+                }
+                else if (firstletter >= 'P' && firstletter <= 'R')
+                {
+                    if (FRESHMAN_radioButton.Checked)
+                        result_label.Text += "at 4:00pm";
+                    else if (SOPHOMORE_radioButton.Checked)
+                        result_label.Text += "at 4:00pm";
+                }
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+
+                           
+    
+
